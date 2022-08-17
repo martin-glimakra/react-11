9b84c0ceeb8ccafc888d48cab047438b080340c7
martin-glimakra <88892064+martin-glimakra@users.noreply.github.com>
2022-08-17 09:20:40 +0200

    Add files via upload

---
+++ Frisör.js
@@ -0,0 +1,11 @@
+const Frisör = ({namn, tid}) => {
+    //javascript kod
+    return (
+        <div>
+            <p>Frisör:{ namn }</p>
+            <p>Tid: {tid}</p>
+        </div>
+    );
+}
+ 
+export default Frisör ;
