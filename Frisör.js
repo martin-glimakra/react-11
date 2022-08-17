aba9b718c9e1c781140e723d2d1fa5a79cf8f7f9
martin-glimakra <88892064+martin-glimakra@users.noreply.github.com>
2022-08-17 10:36:40 +0200

    Update Frisör.js

--- Frisör.js
+++ Frisör.js
@@ -1,9 +1,18 @@
-const Frisör = ({namn, tid}) => {
+const Frisör = ({namn, tid, ledig}) => {
     //javascript kod
+
+    console.log(namn, 'är ledig', ledig)
+
     return (
         <div>
             <p>Frisör:{ namn }</p>
             <p>Tid: {tid}</p>
+            { ledig ? 'jag är ledig' : 'jag är inte ledig' }
+            { ledig ? <div> jag är ledig</div> : <h1> jag är inte ledig </h1> }
+
+            { ledig ? <div className="grön-bakgrund"> jag är ledig</div> : <h1 className="röd-bakgrund"> jag är inte ledig </h1> }
+
+
         </div>
     );
 }
