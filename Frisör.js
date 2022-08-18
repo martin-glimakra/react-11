8c9fe1912f87850dc6f8e6bc23e3d436aa5ef6dd
martin-glimakra <88892064+martin-glimakra@users.noreply.github.com>
2022-08-18 11:00:14 +0200

    Update Frisör.js

--- Frisör.js
+++ Frisör.js
@@ -1,18 +1,24 @@
-const Frisör = ({namn, tid, ledig}) => {
-    //javascript kod
+const Frisör = ({namn, tid, ledig, bildUrl, språk}) => {
 
-    console.log(namn, 'är ledig', ledig)
+    // console.log('språk', språk)
+
+    let ledigText = 'jag är ledig'
+    let inteLedigText = 'jag är inte ledig'
+
+    if (språk === 'engelska') {
+        ledigText = 'I am free'
+        inteLedigText = 'I am not free'
+    }
+
+
 
     return (
         <div>
             <p>Frisör:{ namn }</p>
             <p>Tid: {tid}</p>
-            { ledig ? 'jag är ledig' : 'jag är inte ledig' }
-            { ledig ? <div> jag är ledig</div> : <h1> jag är inte ledig </h1> }
-
-            { ledig ? <div className="grön-bakgrund"> jag är ledig</div> : <h1 className="röd-bakgrund"> jag är inte ledig </h1> }
-
-
+            { ledig ? ledigText : inteLedigText }
+            {bildUrl ? <img src={bildUrl} alt="exempelbild" width="500" height="600"/> : '' }
+            
         </div>
     );
 }
